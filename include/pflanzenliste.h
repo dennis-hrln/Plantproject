abe86d6782298fe4eeb1dca64c60fa08830e52e2
Dennis <canstatter.dennis@gmail.com>
2025-03-04 21:11:35 +0100

    changed motor pin to match test build

--- include/pflanzenliste.h
+++ include/pflanzenliste.h
@@ -2,7 +2,7 @@
 
 //Zimmerpflanze
 const int moisturmeterPnin_zimmerpflanze = A5;  // Moisture sensor pin A0 is an example value
-const int motorPin_zimmerpflanze = LED_BUILTIN;           // Motor pin 9 is an example value
+const int motorPin_zimmerpflanze = 8;           // Motor pin 8 is an example value
 const int SD_card_pin_zimmerpflanze = 4; // SD card pin 4 is an example value
 
 plant Zimmerpflanze("Zimmerpflanze", 30.0f, moisturmeterPnin_zimmerpflanze,
