46e5d471223386b769c2e516821ad511dacef9e9
Dennis <canstatter.dennis@gmail.com>
2025-03-04 01:05:22 +0100

    updated to match the new class elements

--- include/pflanzenliste.h
+++ include/pflanzenliste.h
@@ -3,7 +3,9 @@
 //Zimmerpflanze
 const int moisturmeterPnin_zimmerpflanze = A0;  // Moisture sensor pin A0 is an example value
 const int motorPin_zimmerpflanze = LED_BUILTIN;           // Motor pin 9 is an example value
-plant Zimmerpflanze("Zimmerpflanze", 20.0f, moisturmeterPnin_zimmerpflanze, motorPin_zimmerpflanze);
+const int SD_card_pin_zimmerpflanze = 4; // SD card pin 4 is an example value
+plant Zimmerpflanze("Zimmerpflanze", 20.0f, moisturmeterPnin_zimmerpflanze,
+     motorPin_zimmerpflanze, SD_card_pin_zimmerpflanze);
 
 
 //Kaktuss
