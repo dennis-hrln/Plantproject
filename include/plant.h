aa63e2ab6f909377b1aab2f676ff9d065aa10398
Dennis <canstatter.dennis@gmail.com>
2025-03-04 01:07:47 +0100

    edited the header to match the new cpp

--- include/plant.h
+++ include/plant.h
@@ -7,25 +7,28 @@
 class plant
 {
     public:
-    plant(String planttype, float optimal_humidity = 25, const int sensor_pin = NULL, const int motor_pin = LED_BUILTIN);
+    plant(String planttype, float optimal_humidity = 25, const int sensor_pin = NULL, const int motor_pin = LED_BUILTIN, const int SD_card_pin = NULL);
 
     
     String planttype;
     float optimal_humidity;
     float humidity;
     float humidity_difference;
-    int sensor_pin;
-    int motor_pin;
+    
 
     void watering(float,int);
     float measure_humidity();
-    void write_to_SDcard(const int,bool);
-    void write_to_SDcard(const int, unsigned long = 60000);
+    void write_to_SDcard(unsigned long = 60000);
 
     private:
+    int sensor_pin;
+    int motor_pin;
+    int SD_card_pin;
     File file;
     unsigned long last_data_write;
-
+    const String data_names[5] = {"time", "planttype", "optimal_humidity", "humidity", "watered"};;
+    unsigned long last_watered;
+    bool watered;
 };
 
 #endif // PLANT_H
