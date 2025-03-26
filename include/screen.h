7cf7c9ffc2fdff3d25bf8559e7bb6f8852b015d4
Dennis <canstatter.dennis@gmail.com>
2025-03-26 05:51:42 +0100

    added button change display logic

--- include/screen.h
+++ include/screen.h
@@ -13,62 +13,70 @@ class screen
     LiquidCrystal_I2C* lcd;
     
     void home_disp(String plantname, int humidity, int optimal_humidity);   // Display humidity /optimal humdiity
-    void water_disp();      // Display the last time the plant was watered
+    void water_disp(unsigned long);      // Display the last time the plant was watered
     void calibration_disp();
-    unsigned long stay_awake_time = 40 * 1000;  // 40 seconds
+    unsigned long stay_awake_time = 60 * 1000;  // in ms
     void screen_dimming();  // Turns off the backlight after a certain time
     void innit();
     unsigned long last_disp_action = 0;
     String disp_status;
-    //todo if buttin pushed : last_disp_activation = millis();
-    //disp.home
-    // - add to every button function
+    unsigned long last_disp_change = 0;
+    bool lit = true;
 
     private:
     //Custom characters
     
-    byte right_arrow[8] = {
-        0b00000,
-        0b00000,
-        0b00000,
-        0b00010,
-        0b11111,
-        0b00010,
-        0b00000,
-        0b00000
-    };
-    byte down_arrow[8] = {
-        0b00100,
-        0b00100,
-        0b00100,
-        0b00100,
-        0b00100,
-        0b11111,
-        0b01110,
-        0b00100
-    };
-    byte up_arrow[8] = {
-        0b00100,
-        0b01110,
-        0b11111,
-        0b00100,
-        0b00100,
-        0b00100,
-        0b00100,
-        0b00100
-    };
-    byte left_arrow[8] = {
-        0b00000,
-        0b00000,
-        0b00000,
-        0b01000,
-        0b11111,
-        0b01000,
-        0b00000,
-        0b00000
-    };
-
-    unsigned long last_disp_change = 0;
+    // byte right_arrow[8] = {
+    //     0b00000,
+    //     0b00000,
+    //     0b00000,
+    //     0b00010,
+    //     0b11111,
+    //     0b00010,
+    //     0b00000,
+    //     0b00000
+    // };
+    // byte down_arrow[8] = {
+    //     0b00100,
+    //     0b00100,
+    //     0b00100,
+    //     0b00100,
+    //     0b00100,
+    //     0b11111,
+    //     0b01110,
+    //     0b00100
+    // };
+    // byte up_arrow[8] = {
+    //     0b00100,
+    //     0b01110,
+    //     0b11111,
+    //     0b00100,
+    //     0b00100,
+    //     0b00100,
+    //     0b00100,
+    //     0b00100
+    // };
+    // byte left_arrow[8] = {
+    //     0b00000,
+    //     0b00000,
+    //     0b00000,
+    //     0b01000,
+    //     0b11111,
+    //     0b01000,
+    //     0b00000,
+    //     0b00000
+    // };
+    byte select_char[8] = {
+        B00000,
+        B00100,
+        B01110,
+        B11111,
+        B11111,
+        B01110,
+        B00100,
+        B00000
+      };
+    
 };
 
 #endif // SCREEN_H
--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,33 @@
 #include <LiquidCrystal_I2C.h>
 #include <screen.cpp>
 
-
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-
 screen lcd_screen(&lcd);
 
 void humidity_control(plant *);
+void calibration();
 void screen_loop(unsigned long, unsigned long, unsigned long);
+void next_button();
+void select_button();
 unsigned long data_frequency = 5000; // time in ms between data writes to the SD card
-int update_time = 10000; // time in ms between updates: how long the arduino waits bewteen loops
+volatile bool next_button_pressed = false;
+volatile bool select_button_pressed = false;
+const byte nextButtonPIN = 2;
+const byte selectButtonPIN = 3;
+bool dry_calibrated = true;
+bool wet_calibrated = true;
 
+void check_next_button()
+{
+  next_button_pressed = true;
+  Serial.println("next button pressed");
+  
+}
+void check_select_button()
+{
+  Serial.println("select button pressed");
+  select_button_pressed = true;
+}
 
 int wet_sensor_value = 186;
 int dry_sensor_value = 513;
@@ -31,22 +48,33 @@ void setup()
   pinMode(Zimmerpflanze.sensor_pin, INPUT);
   pinMode(Zimmerpflanze.motor_pin, OUTPUT);
   pinMode(Zimmerpflanze.SD_card_pin, OUTPUT);
+  pinMode(nextButtonPIN, INPUT_PULLUP);
+  pinMode(selectButtonPIN, INPUT_PULLUP);
 
+  attachInterrupt(digitalPinToInterrupt(nextButtonPIN), check_next_button, RISING);
+  attachInterrupt(digitalPinToInterrupt(selectButtonPIN), check_select_button, RISING);
   // to calibrate the sensor remove the two int in the function
-  Zimmerpflanze.calibrate_humidity_sensor(wet_sensor_value,dry_sensor_value);
+  Zimmerpflanze.calibrate_humidity_sensor(wet_sensor_value, dry_sensor_value);
 }
 
 void loop()
 {
   lcd_screen.screen_dimming();
-  
 
   Zimmerpflanze.write_to_SDcard(data_frequency);
   humidity_control(&Zimmerpflanze);
-  screen_loop(15000, 10000, 45000);
-  delay(update_time);
+  // screen_loop(15000, 10000, 45000);
   Zimmerpflanze.write_to_pc(data_frequency);
-  lcd_screen.last_disp_action = millis(); //simiulating button press
+  calibration();
+  if (next_button_pressed)
+  {
+    next_button();
+  }
+  if (select_button_pressed)
+  {
+    select_button();
+  }
+  // delay(1000);
 }
 
 void humidity_control(plant *Pflanze)
@@ -55,35 +83,117 @@ void humidity_control(plant *Pflanze)
   if (h_diff > 0)
   {
     Pflanze->watering(h_diff, 100);
-    // Serial.println("The humidity is ");
-    // Serial.println(Pflanze->humidity);
-  }
-  else
-  {
-    // Serial.println("The humidity is ");
-    // Serial.println(Pflanze->humidity);
   }
 }
 
-void screen_loop(unsigned long looping_time_home = 15 *1000, unsigned long looping_time_watering = 10 *1000, unsigned long max_page_time = 45*1000)  // time in ms between updates: how long the arduino waits bewteen loops
+// void screen_loop(unsigned long looping_time_home = 15 * 1000, unsigned long looping_time_watering = 10 * 1000, unsigned long max_page_time = 45 * 1000) // time in ms between updates: how long the arduino waits bewteen loops
+// {
+//   if ((millis() - lcd_screen.last_disp_change > looping_time_home) && (lcd_screen.disp_status == "home"))
+
+//   {
+//     lcd_screen.water_disp(Zimmerpflanze.last_watered);
+//   }
+//   else if ((millis() - lcd_screen.last_disp_change > looping_time_watering) && (lcd_screen.disp_status == "water_disp"))
+//   {
+//     lcd_screen.home_disp(Zimmerpflanze.planttype, Zimmerpflanze.humidity, Zimmerpflanze.optimal_humidity);
+//   }
+//   else if (lcd_screen.disp_status == "startup")
+//   {
+//     lcd_screen.home_disp(Zimmerpflanze.planttype, Zimmerpflanze.humidity, Zimmerpflanze.optimal_humidity);
+//   }
+//   else if ((millis() - lcd_screen.last_disp_change > max_page_time))
+//   {
+//     lcd_screen.home_disp(Zimmerpflanze.planttype, Zimmerpflanze.humidity, Zimmerpflanze.optimal_humidity);
+//   }
+// }
+
+void calibration()
 {
-    if ((millis() - lcd_screen.last_disp_action > looping_time_home) && (lcd_screen.disp_status == "home"))
-    
+  if (dry_calibrated == false)
+  {
+    lcd_screen.lcd->clear();
+    lcd_screen.lcd->setCursor(0, 0);
+    lcd_screen.lcd->print("Dry calibration");
+    lcd_screen.lcd->setCursor(0, 1);
+    lcd_screen.lcd->print("wait 30s");
+    Zimmerpflanze.humidity_sensor_dry_calibration();
+    dry_calibrated = true;
+    lcd_screen.lcd->clear();
+    lcd_screen.home_disp(Zimmerpflanze.planttype, Zimmerpflanze.humidity, Zimmerpflanze.optimal_humidity);
+  }
+  if (wet_calibrated == false)
+  {
+    lcd_screen.lcd->clear();
+    lcd_screen.lcd->setCursor(0, 0);
+    lcd_screen.lcd->print("Wet calibration");
+    lcd_screen.lcd->setCursor(0, 1);
+    lcd_screen.lcd->print("wait 30s");
+    Zimmerpflanze.humidity_sensor_wet_calibration();
+    wet_calibrated = true;
+    lcd_screen.lcd->clear();
+    lcd_screen.home_disp(Zimmerpflanze.planttype, Zimmerpflanze.humidity, Zimmerpflanze.optimal_humidity);
+  }
+}
+
+void next_button()
+{
+  if (digitalRead(nextButtonPIN) == HIGH)
+  {
+    lcd_screen.last_disp_action = millis();
+
+    if (lcd_screen.lit == false)
     {
-      lcd_screen.water_disp();
-    }
-    else if ((millis() - lcd_screen.last_disp_action > looping_time_watering) && (lcd_screen.disp_status == "water_disp"))
-    {
-      lcd_screen.home_disp(Zimmerpflanze.planttype, Zimmerpflanze.humidity, Zimmerpflanze.optimal_humidity);
-    }
-    else if (lcd_screen.disp_status == "startup")
-    {
-      lcd_screen.home_disp(Zimmerpflanze.planttype, Zimmerpflanze.humidity, Zimmerpflanze.optimal_humidity);
-    }
-    else if ((millis() - lcd_screen.last_disp_action > max_page_time))
-    {
-      lcd_screen.home_disp(Zimmerpflanze.planttype, Zimmerpflanze.humidity, Zimmerpflanze.optimal_humidity);
+      lcd_screen.lcd->backlight();
+      lcd_screen.lit = true;
     }
 
+    if (lcd_screen.disp_status == "home")
+    {
+      lcd_screen.water_disp(Zimmerpflanze.last_watered);
+    }
+    else if (lcd_screen.disp_status == "water_disp")
+    {
+      lcd_screen.calibration_disp();
+    }
+    else if (lcd_screen.disp_status == "dry_calibration")
+    {
+      lcd_screen.disp_status = "wet_calibration";
+      lcd_screen.calibration_disp();
+    }
+    else if (lcd_screen.disp_status == "wet_calibration")
+    {
+      lcd_screen.home_disp(Zimmerpflanze.planttype, Zimmerpflanze.humidity, Zimmerpflanze.optimal_humidity);
+    }
+    else
+    {
+      lcd_screen.home_disp(Zimmerpflanze.planttype, Zimmerpflanze.humidity, Zimmerpflanze.optimal_humidity);
+    }
+  }
+  next_button_pressed = false;
+  delay(100);
+}
 
+void select_button()
+{
+  if (digitalRead(selectButtonPIN) == HIGH)
+  {
+    lcd_screen.last_disp_action = millis();
+
+    if (lcd_screen.lit == false)
+    {
+      lcd_screen.lcd->backlight();
+      lcd_screen.lit = true;
+    }
+
+    if (lcd_screen.disp_status == "dry_calibration")
+    {
+      dry_calibrated = false;
+    }
+    else if (lcd_screen.disp_status == "wet_calibration")
+    {
+      wet_calibrated = false;
+    }
+  }
+  select_button_pressed = false;
+  delay(100);
 }
