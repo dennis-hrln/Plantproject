22caa3c181b34182855b115ecafc4223bc517e14
Dennis <canstatter.dennis@gmail.com>
2025-04-21 02:52:40 +0200

    removed snprintf string creation for better memory handling

--- src/sd_card.cpp
+++ src/sd_card.cpp
@@ -9,6 +9,7 @@ SD_Card::SD_Card(int pin, unsigned long data_write_frequency)
 
 void SD_Card::write_to_SDcard(plant *plant_instance, TimeStruct *now)
 {
+
     // the plant instance is used to get the data from the plant class (for example the humidity from Stirps)
     if (millis() - this->last_data_write > this->data_frequency && millis()> 5000)
     {
@@ -26,18 +27,16 @@ void SD_Card::write_to_SDcard(plant *plant_instance, TimeStruct *now)
 
         if (!SD.begin(this->SD_card_pin))
         {
-            // error message
-            Serial.println(F("SD Card initialization failed!"));
+
             return;
         }
-        Serial.println(F("SD Card initialized."));
         // open file for writing
-        
+
         snprintf(filename, sizeof(filename), "%s_%04d.%02d.%02d_data.csv", plant_instance->planttype,
                  Year, Month, Day);
-        Serial.print(filename);
+        // Serial.print(filename);
+
         file = SD.open(filename, FILE_WRITE);
-        Serial.println(F("SD Card opened."));
         // if the file is not initialised yet -> titles for csv file (if init == false)
         if (!this->init)
         {
@@ -45,19 +44,10 @@ void SD_Card::write_to_SDcard(plant *plant_instance, TimeStruct *now)
             if (file)
             {
                 // print the names of the data to the file in csv format
-                for (int i = 0; i < static_cast<int>(sizeof(data_names) / sizeof(data_names[0])); i++)
-                {
-                    file.print(this->data_names[i]);
-                    file.print(F(", "));
-                }
+                file.print(data_names);
                 file.println(); // Add newline after headers
                 init = true;
                 file.close();
-            }
-            else
-            {
-                // error message
-                Serial.println(F("Error wrtiting to SD card!"));
             }
         }
 
@@ -70,28 +60,37 @@ void SD_Card::write_to_SDcard(plant *plant_instance, TimeStruct *now)
                 const char *watered_str = plant_instance->watered ? "true" : "false";
 
                 // print the data to the file in csv format
-                char data[128]; // buffer for the data to be written to the file
                 // create one giant string with all the data in it
-                snprintf(data, sizeof(data), "%04d,%02d,%02d,%02d,%02d,%02d,%s,%02d,%02d,%s",
-                         Year, Month, Day, Hour, Minute, Second,
-                         plant_instance->planttype, plant_instance->optimal_humidity, plant_instance->humidity, watered_str);
+                file.println(Year);
+                file.print(F(", "));
+                file.print(Month);
+                file.print(F(", "));
+                file.print(Day);
+                file.print(F(", "));
+                file.print(Hour);
+                file.print(F(", "));
+                file.print(Minute);
+                file.print(F(", "));
+                file.print(Second);
+                file.print(F(", "));
+                file.print(plant_instance->planttype);
+                file.print(F(", "));
+                file.print(plant_instance->optimal_humidity);
+                file.print(F(", "));
+                file.print(plant_instance->humidity);
+                file.print(F(", "));
+                file.print(watered_str);
 
-                file.println(data);
                 this->last_data_write = millis();
                 plant_instance->watered = false; // reset the watered variable to false after writing to the file
                 file.close(); // close the file after writing to it
             }
-            else
-            {
-                Serial.println(F("Error wrtiting to SD card!"));
-            }
-        }
-        
-    }
+        }    }
     else
     {
-        Serial.println(F("SD Card not initialized!"));
+        // Serial.println(F("SD Card not initialized!"));
     }
+    
 }
 
 void SD_Card::write_to_pc(unsigned long data_frequency, plant *plant_instance)
